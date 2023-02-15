394a7568fd087e583817bfd966aa738d1426ea40
«Ksenia» <ksenia_slys@mail.ru>
2023-02-15 19:36:01 +0300

    Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a1 = num % 100;
+int a2 = a1 / 10;
+
+Console.WriteLine($"Вторая цифра числа {num} - {a2}");
