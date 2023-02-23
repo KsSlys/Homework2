431d4d937a04ecc9a013c987a1a45659fa4595e8
«Ksenia» <ksenia_slys@mail.ru>
2023-02-23 16:56:31 +0300

    Ex13 верное решение

--- Ex13/Program.cs
+++ Ex13/Program.cs
@@ -1,2 +1,13 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+Console.WriteLine("Введите любое число");
+int a = int.Parse(Console.ReadLine());
+if(a <= 99){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{
+    while (a > 1000){
+        a /= 10; // a = a / 10
+    }
+    Console.WriteLine(a%10);
+}
